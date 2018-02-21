9e8c668b655a5f13c95806b8cf627de523609a6a
Viktor Hesselbom <viktor@hesselbom.net>
2018-02-21 17:44:52 +0100

    Reorganize nav-menu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-const navMenu = document.querySelector('.nav-menu')
-const toggler = navMenu.querySelector('.toggler')
-
-toggler.addEventListener('click', () => (
-  navMenu.classList.toggle('-open')
-))
+import './components/nav-menu'
