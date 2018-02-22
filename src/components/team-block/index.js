2890310daa592aa24e9d07cae0268d448913a5e9
Viktor Hesselbom <viktor@hesselbom.net>
2018-02-22 16:05:56 +0100

    Add team block

---
+++ src/components/team-block/index.js
@@ -0,0 +1,14 @@
+import debounce from 'debounce'
+
+const padding = 15
+const block = document.querySelector('.team-block')
+const cardInfos = block.querySelectorAll('.astronaut-card .info')
+const handleHeights = () => {
+  cardInfos.forEach(info => {
+    info.style.height = 'auto'
+    info.style.height = `${info.scrollHeight + padding}px`
+  })
+}
+
+handleHeights()
+window.addEventListener('resize', debounce(handleHeights, 200))
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import 'browsernizr/test/touchevents'
+import 'browsernizr'
 import './components/nav-menu'
+import './components/team-block'
 
 console.log('%cWelcome to Future Memories! 🚀', 'background:#000; color:#fff; padding:0.5em 1em; font-size:1em')
