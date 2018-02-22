248c7927531cd85cabed83a88191e958ff2cdd0f
Viktor Hesselbom <viktor@hesselbom.net>
2018-02-22 16:20:58 +0100

    Some nav menu styling

--- src/components/nav-menu/index.js
+++ src/components/nav-menu/index.js
@@ -1,7 +1,31 @@
+import smoothScroll from 'smoothscroll'
+
 const navMenu = document.querySelector('.nav-menu')
 const toggler = navMenu.querySelector('.toggler')
+const links = navMenu.querySelectorAll('a')
 
 toggler.addEventListener('click', () => {
   navMenu.classList.toggle('-open')
   document.body.classList.toggle('-locked')
 })
+
+for (let i = 0; i < links.length; i++) {
+  const link = links[i]
+  const id = link.href.substr(link.href.indexOf('#') + 1)
+  const target = document.getElementById(id)
+
+  link.addEventListener('click', (ev) => {
+    navMenu.classList.remove('-open')
+    document.body.classList.remove('-locked')
+
+    ev.preventDefault()
+
+    if (window.history.pushState) {
+      window.history.pushState({}, '', '#' + id)
+    }
+
+    if (target) {
+      smoothScroll(target)
+    }
+  })
+}
--- src/components/team-block/index.js
+++ src/components/team-block/index.js
@@ -4,10 +4,11 @@ const padding = 15
 const block = document.querySelector('.team-block')
 const cardInfos = block.querySelectorAll('.astronaut-card .info')
 const handleHeights = () => {
-  cardInfos.forEach(info => {
+  for (let i = 0; i < cardInfos.length; i++) {
+    let info = cardInfos[i]
     info.style.height = 'auto'
     info.style.height = `${info.scrollHeight + padding}px`
-  })
+  }
 }
 
 handleHeights()
