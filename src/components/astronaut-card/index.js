0f4a5b443ead6079395c13a5ed74e32e6d187b88
Viktor Hesselbom <viktor@hesselbom.net>
2018-02-23 11:36:30 +0100

    Make very subtle shine effect on hover

--- src/components/astronaut-card/index.js
+++ src/components/astronaut-card/index.js
@@ -2,18 +2,22 @@ import Modernizr from 'browsernizr'
 
 const cards = document.querySelectorAll('.astronaut-card')
 
-const mouseMove = card => ev => {
+const mouseMove = (card, shine) => ev => {
   const rect = card.getBoundingClientRect()
   const mouseX = ev.clientX - rect.left
   const mouseY = ev.clientY - rect.top
   const x = (0.5 - (mouseX / rect.width)) * 1
   const y = (0.5 - (mouseY / rect.height)) * -1
+  const deg = Math.atan2(-y, x) * 180 / Math.PI + 90
 
   card.style.transform = `perspective(20rem) translateZ(0.1rem) rotateX(${y}deg) rotateY(${x}deg)`
+  shine.style.background = `linear-gradient(${deg}deg, rgba(255, 255, 255, 0.02), rgba(255, 255, 255, 0))`
 }
 
 if (!Modernizr.touchevents) {
   for (let i = 0; i < cards.length; i++) {
-    cards[i].addEventListener('mousemove', mouseMove(cards[i]))
+    const card = cards[i]
+    const shine = card.querySelector('.shine')
+    card.addEventListener('mousemove', mouseMove(card, shine))
   }
 }
