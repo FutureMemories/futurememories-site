b05dae491880ffd6cfa0c95e5b5ff243c9df7819
Viktor Hesselbom <viktor@hesselbom.net>
2018-02-23 13:42:37 +0100

    Fix astronaut focus on mobile

--- src/components/team-block/index.js
+++ src/components/team-block/index.js
@@ -1,13 +1,12 @@
 import debounce from 'debounce'
 
-const padding = 15
 const block = document.querySelector('.team-block')
 const infoBlocks = block.querySelectorAll('.item .info')
 const handleHeights = () => {
   for (let i = 0; i < infoBlocks.length; i++) {
     let info = infoBlocks[i]
     info.style.height = 'auto'
-    info.style.height = `${info.scrollHeight + padding}px`
+    info.style.height = `${info.scrollHeight}px`
   }
 }
 
