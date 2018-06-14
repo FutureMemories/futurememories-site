0bbb5d248473ad38e21bc7770a306db61b386c53
Mattias Lönn <mattias.lonn@hotmail.com>
2018-06-14 12:04:53 +0200

    Fix link to 'cases.html

--- src/components/nav-menu/index.js
+++ src/components/nav-menu/index.js
@@ -15,8 +15,8 @@ if (navMenu) {
 
   for (let i = 0; i < links.length; i++) {
     const link = links[i]
-    const id = link.href.substr(link.href.indexOf('#') + 1)
-    const target = document.getElementById(id)
+    const id = link.href.substr(link.href.lastIndexOf('/') + 1)
+    const target = document.getElementById(id.substr(1))
 
     link.addEventListener('click', (ev) => {
       if (document.location.pathname.length > 1) {
@@ -29,7 +29,10 @@ if (navMenu) {
       ev.preventDefault()
 
       if (window.history.pushState) {
-        window.history.pushState({}, '', '#' + id)
+        if (id.includes('#'))
+          window.history.pushState({}, '', id)
+        else
+          window.location.assign(`/${id}`)
       }
 
       if (target) {
