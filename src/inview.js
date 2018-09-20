73d3dbf6316360038cd6953678be40570cae42e9
Mattias Lönn <mattias.lonn@hotmail.com>
2018-09-20 21:13:46 +0200

    Add transitions to Retts Plus case page

--- src/inview.js
+++ src/inview.js
@@ -2,9 +2,11 @@ import inView from 'in-view'
 
 const selectors = [
   '.text-block',
+  '.text-block-multiple',
   '.bookmark-block .content',
   '.devices-block',
-  '.grid-block.-more-projects'
+  '.grid-block.-more-projects',
+  '.slide-in-block'
 ].join(',')
 
 if (document.body.classList.contains('-case')) {
