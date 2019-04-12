ca1f0d0168f9157e57691ee5858c6fb32a78ea7e
Mattias Lönn <mattias.lonn@hotmail.com>
2019-04-12 15:35:56 +0200

    Update copy for Bauer University

--- src/cases/bauer-university.js
+++ src/cases/bauer-university.js
@@ -13,7 +13,8 @@ import CenterBlock from './components/center-block'
 const inViewClasses = [
   `${s.inner} > div:nth-child(3) > div:first-child > div`,
   `${s.inner} > div:nth-child(4) > div:last-child > div`,
-  `${s.inner} > div:nth-child(5) > div:first-child`
+  `${s.inner} > div:nth-child(5) > div:first-child`,
+  `${s.inner} > div:nth-child(6) > div:first-child > div`
 ].join(',.')
 
 export default class extends Component {
@@ -42,14 +43,14 @@ export default class extends Component {
 
             <CenterBlock
               inView='inViewBottom'
-              title={`Train online,\nmore sales`}
-              text='Gamification-based online training platform. Turns worldwide Bauer retailers into Bauer hockey gear experts'
+              title={`Taking sales from Rookie to Pro`}
+              text='Bauer University is a global learning platform for Bauer retailers. It educates sales personel about Bauer’s assortment and teaches how to help customers finding the right hockey gear for their specific needs.'
             />
 
             <TextGridBlock
               blocks={[
                 { inView: 'inViewLeft', image: 'cases/bauer-university-2.png', alt: 'Bauer University (Sales Training) tablet mockup' },
-                { inView: 'inViewRight', title: 'Taking sales from Rookie to Pro.', desc: `Bauer University is a global teaching resource for Bauer representatives to make them even better at helping all hockey players find the right gear for their specific needs. The platform relies heavily on gamification.` },
+                { inView: 'inViewRight', title: 'Gamification is the carrot, Bauer makes the sticks', desc: `Bauer University helps retailers to become more knowledgeable through product trainings for Bauer equipment and real-world sales scenarios. Motivation is kept up by well thought-out gamification concepts where achievements result in both physical and digital rewards. All wrapped up in an extremely polished web-based experience.` },
                 { inView: 'inViewRight', image: 'cases/bauer-university-3.png', alt: 'Bauer University (Product Category) tablet mockup' }, { inView: 'inViewLeft', image: 'cases/bauer-university-4.png', alt: 'Bauer University (Product Training) tablet mockup' }
               ]}
               background='#151D20'
@@ -61,8 +62,14 @@ export default class extends Component {
               background='#F9F9FA'
               blocks={[
                 { type: 'image', image: 'cases/bauer-university-5.png', modifier: 'matSeIpad' },
-                { type: 'text', title: 'Turbulence is so 2017', text: 'Because the pilots can get a weather report with current winds, they can plan their flight to avoid any turbulence and get a more fuel efficient, smoother journey.' }
+                { type: 'text', title: 'Putting the biscuit in the basket', text: 'Bauer University is the result of bringing UX experts, game designers and developers together. We developed a game-like achievement system with experience points and ranks and built everything from the ground up. The game engine is developed in Python, using well-established Open Source frameworks. The user interface is a React-based web application, making Bauer University accessible to almost any device. Communication between the frontend and game engine happens through our own GraphQL interface.' }
               ]}
+            />
+
+            <CenterBlock
+              inView='inViewBottom'
+              background='#F5F5F5'
+              text={`Bauer University is the result of combining forces with our\ncreative friends over at The Bond Communication. 🖤`}
             />
 
             <ProjectsBlock current='bauer-university' background='#ffffff' />
