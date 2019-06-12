cbb029e9bdb1e3d7b503b0c11d0e4d9dbf7c3acd
Mattias Lönn <mattias.lonn@hotmail.com>
2019-06-12 11:47:08 +0200

    Change Antistress wave color

--- src/cases/antistress.js
+++ src/cases/antistress.js
@@ -58,14 +58,14 @@ export default class extends Component {
                 speed={12}
                 height={155}
                 waves={[
-                  { lineWidth: 1, amplitude: -80, wavelength: 150, strokeStyle: 'rgba(212, 222, 218, 0.5)' },
-                  { lineWidth: 1, amplitude: -55, wavelength: 150, strokeStyle: 'rgba(212, 222, 218, 0.5)' },
-                  { lineWidth: 1, amplitude: -20, wavelength: 150, strokeStyle: 'rgba(212, 222, 218, 0.7)' },
-                  { lineWidth: 1, amplitude: 45, wavelength: 150, strokeStyle: 'rgba(212, 222, 218, 0.5)' },
-                  { lineWidth: 1, amplitude: 20, wavelength: 150, strokeStyle: 'rgba(212, 222, 218, 0.5)' },
-                  { lineWidth: 1, amplitude: 70, wavelength: 150, strokeStyle: 'rgba(212, 222, 218, 0.5)' },
-                  { lineWidth: 1, amplitude: 110, wavelength: 150, strokeStyle: 'rgba(212, 222, 218, 0.5)' },
-                  { lineWidth: 2, amplitude: 150, wavelength: 150, strokeStyle: 'rgba(212, 222, 218, 0.8)' }
+                  { lineWidth: 1, amplitude: -80, wavelength: 150, strokeStyle: 'rgba(151, 203, 222, 0.5)' },
+                  { lineWidth: 1, amplitude: -55, wavelength: 150, strokeStyle: 'rgba(151, 203, 222, 0.5)' },
+                  { lineWidth: 1, amplitude: -20, wavelength: 150, strokeStyle: 'rgba(151, 203, 222, 0.7)' },
+                  { lineWidth: 1, amplitude: 45, wavelength: 150, strokeStyle: 'rgba(151, 203, 222, 0.5)' },
+                  { lineWidth: 1, amplitude: 20, wavelength: 150, strokeStyle: 'rgba(151, 203, 222, 0.5)' },
+                  { lineWidth: 1, amplitude: 70, wavelength: 150, strokeStyle: 'rgba(151, 203, 222, 0.5)' },
+                  { lineWidth: 1, amplitude: 110, wavelength: 150, strokeStyle: 'rgba(151, 203, 222, 0.5)' },
+                  { lineWidth: 2, amplitude: 150, wavelength: 150, strokeStyle: 'rgba(151, 203, 222, 0.8)' }
                 ]}
               />
             </CenterBlock>
