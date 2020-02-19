fc20b8858cd1c370c30f290934e2d7ed73f39267
Viktor Hesselbom <viktor@hesselbom.net>
2020-02-19 11:11:13 +0100

    Add xhtml namespace to sitemap

--- prebuild.js
+++ prebuild.js
@@ -62,12 +62,12 @@ const generateSitemap = () => {
     )
   }, '')
 
-  const sitemap = [
-    '<?xml version="1.0" encoding="UTF-8"?>', '<urlset',
-    '  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"', '  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"',
-    '  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"',
-    '>' + siteUrls, '</urlset>'
-  ].join('\n')
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:xhtml="http://www.w3.org/1999/xhtml"
+>${siteUrls}
+</urlset>`
 
   writeFileSync(resolve(__dirname, 'src/assets-root/sitemap.xml'), sitemap)
 }
